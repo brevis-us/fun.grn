1abefc80c794d3b6868816843f48c0987fb720a3
Kyle Harrington <kharrington@uidaho.edu>
2017-07-02 21:18:54 +0200

    initial

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject fungrneat "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-random "0.1.8"]
+                 [us.brevis/GRNEAT "0.0.3"]]
+  :plugins [[lein-localrepo "0.5.4"]]
+  :repositories [["brevis-bintray" "https://dl.bintray.com/kephale/brevis"]])
