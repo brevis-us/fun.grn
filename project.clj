cbd3a8639ec40faabbb7e3b1323dbd7522d37ad9
Kyle Harrington <kharrington@uidaho.edu>
2017-07-05 20:53:26 +0200

    Version bump

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject fun.grn "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject fun.grn "0.1.0"
+  :description "Gene-regulatory networks in Clojure"
+  :url "https://github.com/kephale/fun.grn"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
