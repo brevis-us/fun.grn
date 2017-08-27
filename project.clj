f7a115574550963cc0365786e19dc90fa571e6f4
Kyle Harrington <kharrington@uidaho.edu>
2017-08-27 12:07:07 -0700

    grneat bump

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-random "0.1.8"]
                  [brevis.us/brevis-utils "0.1.1"]
-                 [fun.grn/grneat "0.0.4"]]
+                 [fun.grn/grneat "0.0.5"]]
   :plugins [[lein-localrepo "0.5.4"]]
   :repositories [["brevis-bintray" "https://dl.bintray.com/kephale/brevis"]
                  ["snapshots" {:url "https://clojars.org/repo"
