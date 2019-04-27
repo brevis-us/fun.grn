9c3f5d4563908445cdf496f292f6c714c5778fce
Kyle IS Harrington <kyle.i.s.harrington@gmail.com>
2019-04-27 11:02:29 -0400

    Fix brevis-utils and grneat dependency

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject fun.grn "0.1.2-SNAPSHOT"
+(defproject us.brevis/fun.grn "0.1.2-SNAPSHOT"
   :description "Gene-regulatory networks in Clojure"
   :url "https://github.com/kephale/fun.grn"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-random "0.1.8"]
-                 [brevis.us/brevis-utils "0.1.1"]
-                 [fun.grn/grneat "0.0.5"]]
-  :plugins [[lein-localrepo "0.5.4"]]
+                 [us.brevis/brevis-utils "eb70f90d49"]
+                 [us.brevis/grneat "2e68743ff7"]]
   :repositories [["brevis-bintray" "https://dl.bintray.com/kephale/brevis"]
+                 ["jitpack" "https://jitpack.io"]
                  ["snapshots" {:url "https://clojars.org/repo"
                      :username :env/CI_DEPLOY_USERNAME
                      :password :env/CI_DEPLOY_PASSWORD
@@ -16,5 +16,4 @@
                  ["releases" {:url "https://clojars.org/repo"
                                      :username :env/CI_DEPLOY_USERNAME
                                      :password :env/CI_DEPLOY_PASSWORD
-                                     :sign-releases false}]
-                 ])
+                                     :sign-releases false}]])
